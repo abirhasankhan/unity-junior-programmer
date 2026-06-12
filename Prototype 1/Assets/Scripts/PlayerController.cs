9bb58c6633e1015cdca2ea542721d6b55920cf5f
Abir Khan <ak99.abirkhan@gmail.com>
2026-06-13 03:11:43 +0600

    add player control

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed = 10.0f;
+    public float turnSpeed = 70.0f;
+    public InputAction moveAction;
+    private Vector2 moveInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveInput = moveAction.ReadValue<Vector2>();
         // Move the vehicle forward
         // transform.Translate(0, 0, 1);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveInput.y); 
+        // transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * moveInput.x);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * moveInput.x);
+
+
     }
 }

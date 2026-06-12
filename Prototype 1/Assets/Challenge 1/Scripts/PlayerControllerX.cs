4a4ea4176347a3e304cfa9defeb3732930351b78
Abir Khan <ak99.abirkhan@gmail.com>
2026-06-13 04:10:47 +0600

    completed challenge 1

--- Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControllerX : MonoBehaviour
 {
@@ -8,22 +9,42 @@ public class PlayerControllerX : MonoBehaviour
     public float rotationSpeed;
     public float verticalInput;
 
+    public InputAction moveAction;
+
+    private Vector2 moveInput;
+
+    public GameObject propeller;
+
+    private Vector3 spin = new Vector3(0, 0, 400);
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        moveInput = moveAction.ReadValue<Vector2>();
         // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+        // verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * moveInput.x);
+
+        // if (moveInput.x != 0)
+        // {
+        //     // tilt the plane up/down based on up/down arrow keys
+        //     transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime * moveInput.y);
+        // }
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime *  moveInput.y);
+
+        propeller.transform.Rotate(Time.deltaTime * spin) ;
+
     }
 }
